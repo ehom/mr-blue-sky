cf4a40737da730d4c72def17bec6256878de2036
ehom <ehom@users.noreply.github.com>
2023-01-31 15:47:45 -0800

    add setup complete message

--- setup.bash
+++ setup.bash
@@ -1,13 +1,20 @@
 #!/usr/bin/bash
 set -x
 git submodule update --init
+
+###
 cd message-lint && git checkout main
 ls
 git branch
 pwd
 cd ..
+
+###
 cd pseudo-loc && git checkout main
 ls
 git branch
 pwd
 cd ..
+pwd
+
+echo "*** setup completed ***"
